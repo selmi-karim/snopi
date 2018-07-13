b85167d12b947ddc655db15a85e20c03469abb52
Selmi11895480 <selmi.kerim@gmail.com>
2018-07-13 17:30:53 +0100

    api dialogflow

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 'use strict';
 
-const APIAI_TOKEN = process.env.APIAI_TOKEN;
-const APIAI_SESSION_ID = process.env.APIAI_SESSION_ID;
-
-
 
 const express = require('express');
 const app = express();
 const request = require('request');
+require('dotenv').config();
 
 app.use(express.static(__dirname + '/public')); // js, css, images
 
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
+
+const APIAI_TOKEN = process.env.APIAI_TOKEN;
+const APIAI_SESSION_ID = process.env.APIAI_SESSION_ID;
+
 
 const io = require('socket.io')(server);
 io.on('connection', function(socket){
